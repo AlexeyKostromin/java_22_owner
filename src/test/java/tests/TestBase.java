4e7478fa934c45bb7a2e31e5b9ddb281056eb797
Alexey Kostromin <kostromin.aleksei@gmail.com>
2023-11-13 22:09:09 +0100

    Set capability Map.of

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,14 +14,18 @@ public class TestBase {
         Configuration.browserSize = "1920x1280";
         Configuration.baseUrl = "https://demoqa.com";
         //Configuration.pageLoadStrategy="eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object> of(
+//        capabilities.setCapability("selenoid:options", Map.<String, Object> of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+
+        capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
         Configuration.browserCapabilities = capabilities;
 
 //        clearBrowserCookies();
