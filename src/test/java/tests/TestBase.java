a07a1232b558aa9f7046ebaf52eda5d0d64f775b
Alexey Kostromin <kostromin.aleksei@gmail.com>
2023-11-15 22:10:23 +0100

    added getProperties to build.gradle

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1280";
         Configuration.baseUrl = "https://demoqa.com";
         //Configuration.pageLoadStrategy="eager";
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 //        capabilities.setCapability("selenoid:options", Map.<String, Object> of(
